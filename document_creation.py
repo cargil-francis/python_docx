06ce28281f604c87a13daecb1c8090a439d94357
cargil-francis <kitty.f9876@gmail.com>
2024-04-27 22:44:43 +0530

    added title to new doc

--- document_creation.py
+++ document_creation.py
@@ -1,4 +1,13 @@
+#imports
 from docx import Document
 from docx.shared import pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+#create new document
+doc = Document()
+#add title
+title = doc.add_heading ("my new documet sample1", level=2)
+title.alignment = WD_ALIGN_PARAGRAPH.LEFT
+
+
+
 
